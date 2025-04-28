c40debac5e454fd539936d1b96a269c3e2919b4c
Frederick Eaton <frederik@ofb.net>
2025-04-28 10:40:58 -0700

    Add seeking, print word start offsets.

--- list-word2vec-bin.c
+++ list-word2vec-bin.c
@@ -7,6 +7,8 @@
 #define MAX_WORD_LEN 100
 
 long int total_read = 0;
+long int can_seek = 1;
+long int j=0;
 
 int my_getchar() {
   total_read++;
@@ -16,7 +18,23 @@ int my_fread(int length, void *dummy) {
   total_read += length;
   return fread(dummy, length, 1, stdin);
 }
+#define MAX_READ 1024*4
+char dummy_buffer[MAX_READ];
 
+int my_fseek(int length) {
+  if(can_seek) {
+    total_read += length;
+    return fseek(stdin, length, SEEK_CUR);
+  } else {
+    /* fprintf(stderr, "simulating seek of length %d\n", length); */
+    while(length>MAX_READ) {
+      int res = my_fread(MAX_READ, dummy_buffer);
+      if(res==-1) return res;
+      length -= MAX_READ;
+    }
+    return my_fread(length, dummy_buffer);
+  }
+}
 
 int main() {
   long vocab_size, vector_size;
@@ -25,6 +43,11 @@ int main() {
   long int i;
   int scan_count;
 
+  if (ftell(stdin) == -1) {
+    fprintf(stderr, "Warning: STDIN may not be seekable: %s\n",
+      strerror(errno));
+    can_seek = 0;
+  }
   // Read vocab size and vector size from first line
   if (scanf("%ld %ld\n%n", &vocab_size, &vector_size, &scan_count) != 2) {
     fprintf(stderr, "Failed to read header\n");
@@ -37,9 +60,9 @@ int main() {
 
   // Process each word entry
   /* for (long j = 0; j < vocab_size; j++) { */
-  long int j=0;
   while(!feof(stdin)) {
     // Read word until space
+    long int word_start = total_read;
     i = 0;
     while ((c = my_getchar()) != ' ' && c != EOF) {
       if (i < MAX_WORD_LEN - 1) {
@@ -49,22 +72,10 @@ int main() {
     word[i] = '\0';
         
     // Print the word
-    printf("%ld %ld %s\n", j, total_read, word);
+    printf("%ld %ld %s\n", j, word_start, word);
 
-    if(1) {
-      // This works
-      float dummy[vector_size];
-      if (my_fread(sizeof(float)*vector_size, dummy) != 1) {
-        fprintf(stderr, "Error reading vector data for word %d\n", j);
-        return 1;
-      }
-    } else {
-      // I wanted to do this, but apparently STDIN is not seekable
-      if (fseek(stdin, sizeof(float)*vector_size, SEEK_CUR) != 0) {
-        fprintf(stderr, "Error seeking after word %d: %s\n", j, strerror(errno));
-        return 1;
-      }
-    }
+    my_fseek(sizeof(float)*vector_size);
+
     j++;
   }
 
